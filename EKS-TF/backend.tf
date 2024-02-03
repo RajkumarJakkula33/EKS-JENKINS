028c0dfaf3409e59f761ab61c8877fe5c82a530c
Rajkumar Jakkula <149739168+RajkumarJakkula33@users.noreply.github.com>
2024-02-02 20:56:48 -0500

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "eks-youtube-statefile-83" # Replace with your actual S3 bucket name
+    bucket = "eks-youtube-statefile-837" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
